28d0b1d0868c43a6ef5d337914e5863166ddf058
djoogway <theflaffywaffe@gmail.com>
2025-11-25 23:16:20 -0600

    Initial terraforming

---
+++ terraform-docker-demo/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_network" "demo" {
+  name = "demo-net"
+}
+
+resource "docker_container" "nginx" {
+  name  = "demo-nginx"
+  image = "nginx:latest"
+
+  networks_advanced {
+    name = docker_network.demo.name
+  }
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:16"
+  keep_locally = false
+}
+
+resource "docker_volume" "postgres_data" {
+  name = "postgres-data-demo"
+}
+
+resource "docker_container" "postgres" {
+  name = "postgres-demo"
+  image = docker_image.postgres.image_id
+
+  networks_advanced {
+    name = docker_network.demo.name
+  }
+
+  env = [
+    "POSTGRES_USER=postgres",
+    "POSTGRES_PASSWORD=postgres",
+    "POSTGRES_DB=demo_db"
+  ]
+
+  mounts {
+    target = "/var/lib/postgresql/data"
+    source = docker_volume.postgres_data.name
+    type = "volume"
+  }
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+}
